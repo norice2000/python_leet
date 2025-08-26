efb80ad5af73bb76764ba52f69de3822b2684ccb
Phong <phongmchau@gmail.com>
2025-08-26 13:45:13 +1000

    back to tidy

---
+++ lists_prac.py
@@ -0,0 +1,16 @@
+# Create a sample collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# creating a collection
+active_users = {}
+for user, status in users.items():
+    print(f"user: {user}, status: {status}")
+    if status == "active":
+        active_users[user] = status
+
+    print(active_users)
