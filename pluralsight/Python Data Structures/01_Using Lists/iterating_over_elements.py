9795873d35e72caa69ca7170c777d5f00bdf1183
Phong <phongmchau@gmail.com>
2025-09-11 15:16:42 +1000

    done

--- pluralsight/Python Data Structures/01_Using Lists/iterating_over_elements.py
+++ pluralsight/Python Data Structures/01_Using Lists/iterating_over_elements.py
@@ -6,3 +6,15 @@ for instrument in instruments:
 # instrument: guitar
 # instrument: piano
 # instrument: drums
+
+
+# Using enumerate() to look with index and value
+item = ["mouse", "keyboard", "monitor"]
+
+for index, key in enumerate(item):
+    print(f"Index: {index + 1}, Item: {key}") # we have to increment index since it starts at 0
+
+#outputs
+# Index: 1, Item: mouse
+# Index: 2, Item: keyboard
+# Index: 3, Item: monitor
