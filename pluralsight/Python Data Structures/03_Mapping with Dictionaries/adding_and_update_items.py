08e205775dbf47906a2cfe2d5af3ce2b76858484
Phong <phongmchau@gmail.com>
2025-09-14 11:30:10 +1000

    done

--- pluralsight/Python Data Structures/03_Mapping with Dictionaries/adding_and_update_items.py
+++ pluralsight/Python Data Structures/03_Mapping with Dictionaries/adding_and_update_items.py
@@ -9,3 +9,21 @@ books['book_2'] = 'cookbook'
 print(f"addded cookbook: {books}")
 books['book_1'] = 'linuxbook'
 print(f"change dict for book_1: {books}")
+
+# Updating multiple of dictionary with `update()`
+settings ={
+    "volume": 22,
+    "language": "english"
+}
+
+override_settings = {
+    "volume": 40,               #changing existing value
+    "tv_show": "demon_slayer"   # adding a new key value
+}
+
+settings.update(override_settings)
+# new python version now has the MERGE operator which is | or =|
+new_settings = settings | override_settings
+print(f"merging method: {new_settings}")
+print(settings)
+# output: {'volume': 40, 'language': 'english', 'tv_show': 'demon_slayer'}
