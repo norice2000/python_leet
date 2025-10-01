932088a0d7ffa367f1d7d1d13b79f34469cc0466
Phong <phongmchau@gmail.com>
2025-10-02 00:19:48 +1000

    done

---
+++ claude_DSA/5.Tree_BFS/tutor.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def levelOrder(root):
+    res = []
+    if not root:
+        return res
+    queue = deque([root])
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        res.append(level)
+    return res
